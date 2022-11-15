7349bc65280a2d4812a2b336a16089dfae26397d
yuliiamaiboroda <111747754+yuliiamaiboroda@users.noreply.github.com>
2022-11-15 19:14:29 +0200

    create modal window for second page

--- src/js/refs.js
+++ src/js/refs.js
@@ -5,4 +5,7 @@ export const refs = {
   galleryLib: document.querySelector('.js-movie-gallery-library'),
   searchBtn: document.querySelector('.search__form'),
   errorString: document.querySelector('.p__header'),
+  backdropEl: document.querySelector('.backdrop'),
+  filmCardEl: document.querySelector('.modal__container'),
+  closeBtnEl: document.querySelector('.modal-btn-close')
 };
