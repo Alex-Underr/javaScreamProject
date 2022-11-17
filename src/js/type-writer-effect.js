a74c5b75596a1dff9f93ebc67bd0976903491943
Iaroslav Seniuk <savuk7@icloud.com>
2022-11-17 15:49:26 +0200

    fix notification

--- src/js/type-writer-effect.js
+++ src/js/type-writer-effect.js
@@ -7,6 +7,22 @@ export function typeWriterEffect(name) {
     const timer = document.querySelector('.type');
     setTimeout(function () {
       timer.style.opacity = 1;
-    }, 1000);
+    }, 300);
   });
+}
+
+export function typeWriterEffectWatched(name) {
+  refs.notifi.innerHTML = createNotificationMarkup(name);
+  const timer = document.querySelector('.type');
+  setTimeout(function () {
+    timer.style.opacity = 1;
+  }, 300);
+}
+
+export function typeWriterEffectQueue(name) {
+  refs.notifi.innerHTML = createNotificationMarkup(name);
+  const timer = document.querySelector('.type');
+  setTimeout(function () {
+    timer.style.opacity = 1;
+  }, 300);
 }
