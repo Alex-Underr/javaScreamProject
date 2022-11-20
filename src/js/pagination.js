418ec21b7293555fbb0eca46d2566073d5f2d416
Iaroslav Seniuk <savuk7@icloud.com>
2022-11-20 23:52:44 +0200

    some fix

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -36,25 +36,6 @@ const options = {
   },
 };
 
-// export const pagination = new Pagination(container, options);
-// pagination.on('beforeMove', function () {
-//   refs.gallery.innerHTML = '';
-// });
-// pagination.on('afterMove', async function (evt) {
-//   try {
-//     Notiflix.Loading.pulse();
-//     const {
-//       data: { results },
-//     } = await fetchTrendingFilms(evt.page);
-//     refs.gallery.innerHTML = createMarkupCards(results);
-
-//     pagination.on('afterMove', scrollToTop);
-//     Notiflix.Loading.remove(250);
-//   } catch (err) {
-//     console.log(err, 'error here');
-//   }
-// });
-
 export const pagination = new Pagination(container, options);
 pagination.on('beforeMove', async function (evt) {
   try {
@@ -78,58 +59,3 @@ pagination.on('beforeMove', async function (evt) {
   }
 });
 pagination.on('afterMove', scrollToTop);
-
-// export const paginationSearch = new Pagination(container, options);
-// let inputValue;
-
-// export async function onFormInput(evt) {
-//   inputValue = evt.target.value;
-//   evt.preventDefault();
-//   if (evt.target.value === '') {
-//     return searchFilmData();
-//   }
-//     if (inputValue === ' ') {
-//       return;
-//   }
-
-//   refs.gallery.textContent = '';
-
-//   paginationSearch.reset();
-//   let page = 1;
-//   const moviesByKeyWord = await fetchSearchFilms(inputValue, page);
-//   const loadGenres = await fetchTrendingFilms();
-//   if (moviesByKeyWord.total_results === 0) {
-//     console.log();
-//   }
-//   if (moviesByKeyWord.total_results > 0) {
-//     console.log(moviesByKeyWord.total_results);
-//   }
-//   container.classList.remove('visually-hidden');
-
-//   createMarkupCards(moviesByKeyWord, fetchTrendingFilms);
-
-//   if (moviesByKeyWord.total_results < 20) {
-//     container.classList.add('visually-hidden');
-//   } else {
-//     container.classList.remove('visually-hidden');
-//   }
-// };
-
-// paginationSearch.on('beforeMove', function (evt) {
-//   refs.gallery.innerHTML = '';
-// });
-// paginationSearch.on('afterMove', async function (evt) {
-
-//   try {
-//     Notiflix.Loading.pulse();
-//     const {
-//       data: { results },
-//     } = await fetchSearchFilms(inputValue, evt.page);
-//     refs.gallery.innerHTML = createMarkupCards(results);
-
-//     Notiflix.Loading.remove(250);
-//   } catch (err) {
-//     console.log(err, 'error here');
-//   }
-
-// });
