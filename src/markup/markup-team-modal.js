ce5bb2f96a5350423969baf7fd9414e1a66f6498
olegtremba1234 <olegtremba@gmail.com>
2022-11-21 12:14:05 +0200

    rebuild markup modal team on js

---
+++ src/markup/markup-team-modal.js
@@ -0,0 +1,212 @@
+export function createTeamModalMarkup() {
+    return `<div class="modal-team" data-team-modal>
+    <button type="button" class="team-close-btn js-close" data-team-close>
+      <svg
+        width="30"
+        height="30"
+        viewBox="0 0 30 30"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="modal-btn-close js-close"
+      >
+        <path d="M8 8L22 22" stroke="black" stroke-width="2" />
+        <path d="M8 22L22 8" stroke="black" stroke-width="2" />
+      </svg>
+    </button>
+    <div class="modal-content">
+      <h1 class="title-team">
+        <div class="title-wrapper">
+          Java <for>Scream </for><for>Скриптизери</for> Team
+        </div>
+      </h1>
+      <ul class="list-team">
+        <li class="team-item">
+          <a
+            href="https://github.com/Alex-Underr"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div class="team-box">
+              <img
+                class="avatar"
+                src="https://i.postimg.cc/QNmhxKJm/team-oleksandr-varakuta.jpg"
+                alt="Cat"
+                width="200px"
+              />
+              <h2 class="team-name">Oleksandr Varakuta</h2>
+              <p class="team-role">Team Lead</p>
+              <div class="git-icon-wrapper">
+                <svg
+                    width="28"
+                    height="28"
+                    viewBox="0 0 32 32"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="git-icon"
+                >
+                    <path d="M16 0c-8.84 0-16 7.16-16 16 0 7.080 4.58 13.060 10.94 15.18 0.8 0.14 1.1-0.34 1.1-0.76 0-0.38-0.020-1.64-0.020-2.98-4.020 0.74-5.060-0.98-5.38-1.88-0.18-0.46-0.96-1.88-1.64-2.26-0.56-0.3-1.36-1.040-0.020-1.060 1.26-0.020 2.16 1.16 2.46 1.64 1.44 2.42 3.74 1.74 4.66 1.32 0.14-1.040 0.56-1.74 1.020-2.14-3.56-0.4-7.28-1.78-7.28-7.9 0-1.74 0.62-3.18 1.64-4.3-0.16-0.4-0.72-2.040 0.16-4.24 0 0 1.34-0.42 4.4 1.64 1.28-0.36 2.64-0.54 4-0.54s2.72 0.18 4 0.54c3.060-2.080 4.4-1.64 4.4-1.64 0.88 2.2 0.32 3.84 0.16 4.24 1.020 1.12 1.64 2.54 1.64 4.3 0 6.14-3.74 7.5-7.3 7.9 0.58 0.5 1.080 1.46 1.080 2.96 0 2.14-0.020 3.86-0.020 4.4 0 0.42 0.3 0.92 1.1 0.76 6.32-2.12 10.9-8.12 10.9-15.18 0-8.84-7.16-16-16-16z">
+                    </path>
+                </svg>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="team-item">
+          <a
+            href="https://github.com/alinchees"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div class="team-box">
+              <img
+                class="avatar"
+                src="https://ca.slack-edge.com/T03SJHRHKSA-U03TTCM2UDN-72cda95e7735-512"
+                alt="Cat"
+                width="200px"
+              />
+              <h2 class="team-name">Alina Marnopolska</h2>
+              <p class="team-role">Scrum Master</p>
+              <div class="git-icon-wrapper">
+                 <svg
+                    width="28"
+                    height="28"
+                    viewBox="0 0 32 32"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="git-icon"
+                >
+                    <path d="M16 0c-8.84 0-16 7.16-16 16 0 7.080 4.58 13.060 10.94 15.18 0.8 0.14 1.1-0.34 1.1-0.76 0-0.38-0.020-1.64-0.020-2.98-4.020 0.74-5.060-0.98-5.38-1.88-0.18-0.46-0.96-1.88-1.64-2.26-0.56-0.3-1.36-1.040-0.020-1.060 1.26-0.020 2.16 1.16 2.46 1.64 1.44 2.42 3.74 1.74 4.66 1.32 0.14-1.040 0.56-1.74 1.020-2.14-3.56-0.4-7.28-1.78-7.28-7.9 0-1.74 0.62-3.18 1.64-4.3-0.16-0.4-0.72-2.040 0.16-4.24 0 0 1.34-0.42 4.4 1.64 1.28-0.36 2.64-0.54 4-0.54s2.72 0.18 4 0.54c3.060-2.080 4.4-1.64 4.4-1.64 0.88 2.2 0.32 3.84 0.16 4.24 1.020 1.12 1.64 2.54 1.64 4.3 0 6.14-3.74 7.5-7.3 7.9 0.58 0.5 1.080 1.46 1.080 2.96 0 2.14-0.020 3.86-0.020 4.4 0 0.42 0.3 0.92 1.1 0.76 6.32-2.12 10.9-8.12 10.9-15.18 0-8.84-7.16-16-16-16z">
+                    </path>
+                </svg>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="team-item">
+          <a
+            href="https://github.com/yuliiamaiboroda"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div class="team-box">
+              <img
+                class="avatar"
+                src="https://i.postimg.cc/qq1TmWvS/team-yuliia.jpg"
+                alt="Cat"
+                width="200px"
+              />
+              <h2 class="team-name">Yuliia Maiboroda</h2>
+              <p class="team-role">Developer</p>
+              <div class="git-icon-wrapper">
+                 <svg
+                    width="28"
+                    height="28"
+                    viewBox="0 0 32 32"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="git-icon"
+                >
+                    <path d="M16 0c-8.84 0-16 7.16-16 16 0 7.080 4.58 13.060 10.94 15.18 0.8 0.14 1.1-0.34 1.1-0.76 0-0.38-0.020-1.64-0.020-2.98-4.020 0.74-5.060-0.98-5.38-1.88-0.18-0.46-0.96-1.88-1.64-2.26-0.56-0.3-1.36-1.040-0.020-1.060 1.26-0.020 2.16 1.16 2.46 1.64 1.44 2.42 3.74 1.74 4.66 1.32 0.14-1.040 0.56-1.74 1.020-2.14-3.56-0.4-7.28-1.78-7.28-7.9 0-1.74 0.62-3.18 1.64-4.3-0.16-0.4-0.72-2.040 0.16-4.24 0 0 1.34-0.42 4.4 1.64 1.28-0.36 2.64-0.54 4-0.54s2.72 0.18 4 0.54c3.060-2.080 4.4-1.64 4.4-1.64 0.88 2.2 0.32 3.84 0.16 4.24 1.020 1.12 1.64 2.54 1.64 4.3 0 6.14-3.74 7.5-7.3 7.9 0.58 0.5 1.080 1.46 1.080 2.96 0 2.14-0.020 3.86-0.020 4.4 0 0.42 0.3 0.92 1.1 0.76 6.32-2.12 10.9-8.12 10.9-15.18 0-8.84-7.16-16-16-16z">
+                    </path>
+                </svg>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="team-item">
+          <a
+            href="https://github.com/SenYaroslav"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div class="team-box">
+              <img
+                class="avatar"
+                src="https://i.postimg.cc/xCyYmXD0/team-yaroslav.png"
+                alt="Cat"
+                width="200px"
+              />
+              <h2 class="team-name">Yaroslav Seniuk</h2>
+              <p class="team-role">Developer</p>
+              <div class="git-icon-wrapper">
+                 <svg
+                    width="28"
+                    height="28"
+                    viewBox="0 0 32 32"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="git-icon"
+                >
+                    <path d="M16 0c-8.84 0-16 7.16-16 16 0 7.080 4.58 13.060 10.94 15.18 0.8 0.14 1.1-0.34 1.1-0.76 0-0.38-0.020-1.64-0.020-2.98-4.020 0.74-5.060-0.98-5.38-1.88-0.18-0.46-0.96-1.88-1.64-2.26-0.56-0.3-1.36-1.040-0.020-1.060 1.26-0.020 2.16 1.16 2.46 1.64 1.44 2.42 3.74 1.74 4.66 1.32 0.14-1.040 0.56-1.74 1.020-2.14-3.56-0.4-7.28-1.78-7.28-7.9 0-1.74 0.62-3.18 1.64-4.3-0.16-0.4-0.72-2.040 0.16-4.24 0 0 1.34-0.42 4.4 1.64 1.28-0.36 2.64-0.54 4-0.54s2.72 0.18 4 0.54c3.060-2.080 4.4-1.64 4.4-1.64 0.88 2.2 0.32 3.84 0.16 4.24 1.020 1.12 1.64 2.54 1.64 4.3 0 6.14-3.74 7.5-7.3 7.9 0.58 0.5 1.080 1.46 1.080 2.96 0 2.14-0.020 3.86-0.020 4.4 0 0.42 0.3 0.92 1.1 0.76 6.32-2.12 10.9-8.12 10.9-15.18 0-8.84-7.16-16-16-16z">
+                    </path>
+                </svg>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="team-item">
+          <a
+            href="https://github.com/FrancuzAlexandr"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div class="team-box">
+              <img
+                class="avatar"
+                src="https://i.postimg.cc/rsVTNqM5/team-oleksandr-argunov.jpg"
+                alt="Cat"
+                width="200px"
+              />
+              <h2 class="team-name">Oleksandr Argunov</h2>
+              <p class="team-role">Developer</p>
+              <div class="git-icon-wrapper">
+                 <svg
+                    width="28"
+                    height="28"
+                    viewBox="0 0 32 32"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="git-icon"
+                >
+                    <path d="M16 0c-8.84 0-16 7.16-16 16 0 7.080 4.58 13.060 10.94 15.18 0.8 0.14 1.1-0.34 1.1-0.76 0-0.38-0.020-1.64-0.020-2.98-4.020 0.74-5.060-0.98-5.38-1.88-0.18-0.46-0.96-1.88-1.64-2.26-0.56-0.3-1.36-1.040-0.020-1.060 1.26-0.020 2.16 1.16 2.46 1.64 1.44 2.42 3.74 1.74 4.66 1.32 0.14-1.040 0.56-1.74 1.020-2.14-3.56-0.4-7.28-1.78-7.28-7.9 0-1.74 0.62-3.18 1.64-4.3-0.16-0.4-0.72-2.040 0.16-4.24 0 0 1.34-0.42 4.4 1.64 1.28-0.36 2.64-0.54 4-0.54s2.72 0.18 4 0.54c3.060-2.080 4.4-1.64 4.4-1.64 0.88 2.2 0.32 3.84 0.16 4.24 1.020 1.12 1.64 2.54 1.64 4.3 0 6.14-3.74 7.5-7.3 7.9 0.58 0.5 1.080 1.46 1.080 2.96 0 2.14-0.020 3.86-0.020 4.4 0 0.42 0.3 0.92 1.1 0.76 6.32-2.12 10.9-8.12 10.9-15.18 0-8.84-7.16-16-16-16z">
+                    </path>
+                </svg>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="team-item">
+          <a
+            href="https://github.com/olegtremba1234"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div class="team-box">
+              <img
+                class="avatar"
+                src="https://i.postimg.cc/qRd4g8Fb/team-oleg.jpg"
+                alt="Cat"
+                width="200px"
+              />
+              <h2 class="team-name">Oleg Tremba</h2>
+              <p class="team-role">Developer</p>
+              <div class="git-icon-wrapper">
+                 <svg
+                    width="28"
+                    height="28"
+                    viewBox="0 0 32 32"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="git-icon"
+                >
+                    <path d="M16 0c-8.84 0-16 7.16-16 16 0 7.080 4.58 13.060 10.94 15.18 0.8 0.14 1.1-0.34 1.1-0.76 0-0.38-0.020-1.64-0.020-2.98-4.020 0.74-5.060-0.98-5.38-1.88-0.18-0.46-0.96-1.88-1.64-2.26-0.56-0.3-1.36-1.040-0.020-1.060 1.26-0.020 2.16 1.16 2.46 1.64 1.44 2.42 3.74 1.74 4.66 1.32 0.14-1.040 0.56-1.74 1.020-2.14-3.56-0.4-7.28-1.78-7.28-7.9 0-1.74 0.62-3.18 1.64-4.3-0.16-0.4-0.72-2.040 0.16-4.24 0 0 1.34-0.42 4.4 1.64 1.28-0.36 2.64-0.54 4-0.54s2.72 0.18 4 0.54c3.060-2.080 4.4-1.64 4.4-1.64 0.88 2.2 0.32 3.84 0.16 4.24 1.020 1.12 1.64 2.54 1.64 4.3 0 6.14-3.74 7.5-7.3 7.9 0.58 0.5 1.080 1.46 1.080 2.96 0 2.14-0.020 3.86-0.020 4.4 0 0.42 0.3 0.92 1.1 0.76 6.32-2.12 10.9-8.12 10.9-15.18 0-8.84-7.16-16-16-16z">
+                    </path>
+                </svg>
+              </div>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+    </div>`
+}
